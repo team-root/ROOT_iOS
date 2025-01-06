3813c355b3410efbcabcce1023b942940bbd3ad4
circle0802 <hawon9781@gmail.com>
2025-01-06 11:32:13 +0900

    setting :: 프로젝트 생성

---
+++ ROOT/ROOTTests/ROOTTests.swift
@@ -0,0 +1,17 @@
+//
+//  ROOTTests.swift
+//  ROOTTests
+//
+//  Created by 원이 on 1/6/25.
+//
+
+import Testing
+@testable import ROOT
+
+struct ROOTTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
