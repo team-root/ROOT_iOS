192de5044afeff7121cf68439210177fcf2490e8
circle0802 <hawon9781@gmail.com>
2025-01-13 11:45:09 +0900

    feat :: Text 구현

--- ROOT/ROOT/Presentation/Scenes/Login/LoginView.swift
+++ ROOT/ROOT/Presentation/Scenes/Login/LoginView.swift
@@ -2,21 +2,24 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        ZStack {
-            Color(.gray600)
-            VStack {
-                Text("로그인")
-                    .ROOTStyle(.heading3)
-                    .lineSpacing(48)
-                    .foregroundColor(.gray100)
-                    .padding(5)
-                Text("Xquare 계정으로 로그인 해주세요!")
-                    .ROOTStyle(.body4)
-                    .lineSpacing(20)
-                    .foregroundColor(.gray100)
+        NavigationView {
+            ZStack {
+                Color(.gray600)
+                    .ignoresSafeArea()
+                VStack {
+                    Text("로그인")
+                        .ROOTStyle(.heading3)
+                        .foregroundColor(.gray100)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .padding(.bottom, 5)
+                    Text("Xquare 계정으로 로그인 해주세요!")
+                        .ROOTStyle(.body3)
+                        .frame(maxWidth: .infinity, alignment: .leading)
+                        .foregroundColor(.gray100)
+                }
+                .padding(.horizontal, 30)
             }
         }
-        .ignoresSafeArea()
     }
 }
 
