d2342eda82f86012645fe4d5bd74270cf96451ac
circle0802 <hawon9781@gmail.com>
2025-01-28 00:21:35 +0900

    feat :: 전체적인 Layout 잡기

--- ROOT/ROOT/Presentation/Scenes/Login/LoginView.swift
+++ ROOT/ROOT/Presentation/Scenes/Login/LoginView.swift
@@ -7,32 +7,39 @@ struct LoginView: View {
                 Color(.gray600)
                     .ignoresSafeArea()
                 VStack {
-                    Text("로그인")
-                        .rootFont(.heading(.heading3))
-                        .foregroundColor(.gray100)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .padding(.bottom, 5)
-                    Text("Xquare 계정으로 로그인 해주세요!")
-                        .rootFont(.body(.body3))
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                        .foregroundColor(.gray100)
-                    Spacer()
-                }
-                .padding(.vertical, 50)
-                .padding(.horizontal, 30)
+                    VStack {
+                        Text("로그인")
+                            .rootFont(.heading(.heading3))
+                            .foregroundColor(.gray100)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .padding(.bottom, 5)
+                        Text("Xquare 계정으로 로그인 해주세요!")
+                            .rootFont(.body(.body3))
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                            .foregroundColor(.gray100)
+                    }
+                    .padding(.vertical, 50)
+                    .padding(.horizontal, 30)
 
-                VStack(alignment: .leading, spacing: 20) {
-                    ROOTAuthTextField(
-                        title: .constant("아이디"),
-                        text: .constant("아이디"),
-                        placeholder: .constant("아이디를 입력해주세요."),
-                        error: .constant("존재하지 않는 아이디입니다."))
+                    VStack(alignment: .leading, spacing: 20) {
+                        ROOTAuthTextField(
+                            title: .constant("아이디"),
+                            text: .constant("아이디"),
+                            placeholder: .constant("아이디를 입력해주세요."),
+                            error: .constant("존재하지 않는 아이디입니다."))
 
-                    ROOTAuthTextField(
-                        title: .constant("비밀번호"),
-                        text: .constant("비밀번호"),
-                        placeholder: .constant("비밀번호를 입력해주세요."),
-                        error: .constant("비밀번호가 일치하지 않습니다."))
+                        ROOTAuthTextField(
+                            title: .constant("비밀번호"),
+                            text: .constant("비밀번호"),
+                            placeholder: .constant("비밀번호를 입력해주세요."),
+                            error: .constant("비밀번호가 일치하지 않습니다."))
+                        Spacer()
+                    }
+
+                    ROOTButton(title: "로그인", isEnabled: true) {
+                        print("로그인 버튼 클릭")
+                    }
+                    .padding(.bottom, 20)
                 }
             }
         }
