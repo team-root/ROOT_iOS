6daa0825575d89e3d33faa241b24d793482efe40
circle0802 <hawon9781@gmail.com>
2025-01-27 19:45:20 +0900

    feat :: AuthTextField Component

--- ROOT/ROOT/Presentation/Components/TextField/ROOTAuthTextField.swift
+++ ROOT/ROOT/Presentation/Components/TextField/ROOTAuthTextField.swift
@@ -1,7 +1,65 @@
-//
-//  Et.swift
-//  ROOT
-//
-//  Created by 원이 on 1/10/25.
-//
+import SwiftUI
 
+struct ROOTAuthTextField: View {
+    @Binding var title: String
+    @Binding var text: String
+    @Binding var placeholder: String
+    @Binding var error: String
+    
+    public init(
+        title: Binding<String>,
+        text: Binding<String>,
+        placeholder: Binding<String>,
+        error: Binding<String>
+    ) {
+        _title = title
+        _text = text
+        _placeholder = placeholder
+        _error = error
+    }
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 2) {
+            Text(title)
+                .rootFont(.caption(.caption3))
+                .foregroundStyle(.gray100)
+
+            ZStack(alignment: .leading) {
+                if placeholder.isEmpty {
+                    Text(text)
+                        .rootFont(.caption(.caption3))
+                        .foregroundStyle(.gray500)
+                }
+                TextField("", text: $placeholder)
+                    .textFieldStyle(PlainTextFieldStyle())
+                    .padding(16)
+                    .rootFont(.body(.body4))
+                    .foregroundStyle(.gray100)
+                    .background(
+                        RoundedRectangle(cornerRadius: 12)
+                            .fill(Color.gray550)
+                            .overlay(
+                                RoundedRectangle(cornerRadius: 12)
+                                    .stroke(Color.gray300, lineWidth: 1)
+                            )
+                    )
+            }
+
+            Text(error)
+                .rootFont(.caption(.caption4))
+                .foregroundStyle(.error)
+                .hidden()
+        }
+        .padding(.horizontal, 24)
+    }
+}
+
+struct DMSFormTextField_Previews: PreviewProvider {
+    static var previews: some View {
+        ROOTAuthTextField(
+            title: .constant("아이디"),
+            text: .constant(""),
+            placeholder: .constant("아이디를 입력해주세요."),
+            error: .constant("존재하지 않는 아이디 입니다"))
+    }
+}
--- ROOT/ROOT/Presentation/Scenes/Login/LoginView.swift
+++ ROOT/ROOT/Presentation/Scenes/Login/LoginView.swift
@@ -8,16 +8,22 @@ struct LoginView: View {
                     .ignoresSafeArea()
                 VStack {
                     Text("로그인")
-                        .ROOTStyle(.heading3)
+                        .rootFont(.heading(.heading3))
                         .foregroundColor(.gray100)
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .padding(.bottom, 5)
                     Text("Xquare 계정으로 로그인 해주세요!")
-                        .ROOTStyle(.body3)
+                        .rootFont(.body(.body3))
                         .frame(maxWidth: .infinity, alignment: .leading)
                         .foregroundColor(.gray100)
+                    Spacer()
                 }
+                .padding(.vertical, 50)
                 .padding(.horizontal, 30)
+
+                VStack {
+                    
+                }
             }
         }
     }
