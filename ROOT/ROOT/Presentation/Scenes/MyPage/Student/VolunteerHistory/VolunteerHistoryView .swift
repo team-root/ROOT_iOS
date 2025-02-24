fc5f68b26e5d618b98913c6e7c1c6b9b76745731
circle0802 <hawon9781@gmail.com>
2025-02-25 00:37:44 +0900

    feat :: 봉사 활동 내역 SrollView 구현

--- ROOT/ROOT/Presentation/Scenes/MyPage/Student/VolunteerHistory/VolunteerHistoryView .swift
+++ ROOT/ROOT/Presentation/Scenes/MyPage/Student/VolunteerHistory/VolunteerHistoryView .swift
@@ -8,6 +8,35 @@ struct VolunteerHistoryView: View {
             ZStack {
                 Color.gray600
                     .ignoresSafeArea()
+                VStack {
+                    TotalVolunteerTimeView(time: 100)
+                        .padding(.top, 20)
+                        .padding(.bottom, 17)
+                    ScrollView {
+                        LazyVStack(spacing: 8) {
+                            ForEach(0..<6, id: \.self) { _ in
+                                Button(action: {}) {
+                                    HStack {
+                                        Text("활동 내용")
+                                            .rootFont(.body(.body3), color: .gray100)
+                                            .padding(.vertical, 20)
+                                            .padding(.leading, 20)
+                                        Spacer()
+                                        Text("+\(16)")
+                                            .rootFont(.body(.body3), color: .main100)
+                                            .padding(.trailing, 20)
+                                    }
+                                    .frame(maxWidth: .infinity)
+                                    .background(.gray550)
+                                    .cornerRadius(10)
+                                    .disabled(true)
+                                }
+                            }
+                        }
+                    }
+                    Spacer()
+                }
+                .padding(.horizontal, 20)
             }
             .toolbar {
                 ToolbarItem(placement: .principal) {
