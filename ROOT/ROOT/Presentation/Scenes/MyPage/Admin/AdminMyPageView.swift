11e13ce47c1c482c83ab706c412a37eaf29cf58d
circle0802 <hawon9781@gmail.com>
2025-03-02 23:07:04 +0900

    feat :: admin mypage navibar

--- ROOT/ROOT/Presentation/Scenes/MyPage/Admin/AdminMyPageView.swift
+++ ROOT/ROOT/Presentation/Scenes/MyPage/Admin/AdminMyPageView.swift
@@ -1,11 +1,39 @@
 import SwiftUI
 
 struct AdminMyPageView: View {
+    @State private var clickAlertButton: Bool = false
+
     var body: some View {
-        VStack {
-            Text("AdminMyPage Screen")
-                .font(.largeTitle)
-                .padding()
+        NavigationView {
+            ZStack {
+                Color.gray600
+                    .ignoresSafeArea()
+                NavigationLink(isActive: $clickAlertButton) {
+                    AlertView()
+                } label: {
+                    EmptyView()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("마이페이지")
+                        .rootFont(.body(.body1), color: .gray100)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        clickAlertButton = true
+                        print("alert button")
+                    }) {
+                        Image("bell")
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar(clickAlertButton ? .hidden : .visible, for: .tabBar)
         }
     }
+}
+
+#Preview {
+    AdminMyPageView()
 }
