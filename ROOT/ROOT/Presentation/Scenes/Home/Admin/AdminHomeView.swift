bfd9b98d5cb98191ed7d805419b8a23f77461e79
circle0802 <hawon9781@gmail.com>
2025-03-04 20:04:01 +0900

    feat :: admin home publishing

--- ROOT/ROOT/Presentation/Scenes/Home/Admin/AdminHomeView.swift
+++ ROOT/ROOT/Presentation/Scenes/Home/Admin/AdminHomeView.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct AdminHomeView: View {
     @State private var clickAlertButton: Bool = false
-
+    
     var body: some View {
         NavigationView {
             ZStack {
@@ -13,6 +13,62 @@ struct AdminHomeView: View {
                 } label: {
                     EmptyView()
                 }
+                VStack(alignment: .leading, spacing: .zero) {
+                    Text("QR CODE")
+                        .rootFont(.heading(.heading6), color: .gray100)
+                        .padding(.top, 17)
+                        .padding(.bottom, 26)
+                    Button(action: {}) {
+                        HStack(alignment: .center, spacing: 0) {
+                            Image("volunteer")
+                                .padding(19)
+                            
+                            Text("학생이 QR을 찍으면\n자동으로 다른 QR로 변경됩니다!")
+                                .rootFont(.body(.body4), color: .gray100)
+                                .multilineTextAlignment(.leading)
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                        }
+                    }
+                    .background(Color.gray550)
+                    .cornerRadius(10)
+                    .padding(.horizontal, 2)
+                    
+                    ZStack {
+                        GeometryReader { geometry in
+                            let size = geometry.size.width
+                            RoundedRectangle(cornerRadius: 10)
+                                .fill(Color.gray550)
+                                .aspectRatio(1, contentMode: .fit)
+                                .overlay(
+                                    Rectangle()
+                                        .fill(Color.main200)
+                                        .frame(width: 0.34, height: size - 50)
+                                        .position(x: size / 2, y: size / 2)
+                                )
+                                .overlay(
+                                    Rectangle()
+                                        .fill(Color.main200)
+                                        .frame(width: size - 50, height: 0.34)
+                                        .position(x: size / 2, y: size / 2)
+                                )
+                                .overlay(
+                                    Button(action: {}) {
+                                        Text("QR 생성하기")
+                                            .rootFont(.body(.body3), color: .gray700)
+                                            .padding(.vertical, 13)
+                                            .padding(.horizontal, 27)
+                                    }
+                                    .background(Color.main200)
+                                    .cornerRadius(15),
+                                    alignment: .center
+                                )
+                        }
+                    }
+                    .padding(.top, 17)
+                    
+                    Spacer()
+                }
+                .padding(.horizontal, 22)
             }
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
