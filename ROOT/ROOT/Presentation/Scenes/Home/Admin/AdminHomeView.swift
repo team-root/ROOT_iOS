1709f852c9711a53ac935d3c9afdb6331c767417
circle0802 <hawon9781@gmail.com>
2025-03-04 11:18:42 +0900

    feat :: Admin Home NaviBar 구현

--- ROOT/ROOT/Presentation/Scenes/Home/Admin/AdminHomeView.swift
+++ ROOT/ROOT/Presentation/Scenes/Home/Admin/AdminHomeView.swift
@@ -1,11 +1,37 @@
 import SwiftUI
 
 struct AdminHomeView: View {
+    @State private var clickAlertButton: Bool = false
+
     var body: some View {
-        VStack {
-            Text("AdminHome Screen")
-                .font(.largeTitle)
-                .padding()
+        NavigationView {
+            ZStack {
+                Color.gray600
+                    .ignoresSafeArea()
+                NavigationLink(isActive: $clickAlertButton) {
+                    AlertView()
+                } label: {
+                    EmptyView()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Image("logo")
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        clickAlertButton = true
+                        print("alert button")
+                    }) {
+                        Image("bell")
+                    }
+                }
+            }
+            .toolbar(clickAlertButton ? .hidden : .visible, for: .tabBar)
         }
     }
+}
+
+#Preview {
+    AdminHomeView()
 }
