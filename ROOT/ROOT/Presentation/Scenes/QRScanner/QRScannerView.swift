28c4ff2b960a035fb195318d6c849ac58a6c3683
circle0802 <hawon9781@gmail.com>
2025-03-05 14:58:04 +0900

    chore :: 성공시 PopUp창 띄우기

--- ROOT/ROOT/Presentation/Scenes/QRScanner/QRScannerView.swift
+++ ROOT/ROOT/Presentation/Scenes/QRScanner/QRScannerView.swift
@@ -6,23 +6,28 @@ struct QRScannerView: View {
     @State private var showingPopUp = false
     
     var body: some View {
-        CodeScannerView(
-            codeTypes: [.qr],
-            simulatedData: "Some simulated data",
-            completion: handleScan
-        )
+        ZStack {
+            CodeScannerView(
+                codeTypes: [.qr],
+                simulatedData: "Some simulated data",
+                completion: handleScan
+            )
+            if showingPopUp {
+                ROOTPopUpView(
+                    isPresent: $showingPopUp,
+                    title: "출석 체크 완료!",
+                    content: "출석 체크가 완료되었습니다."
+                )
+                .transition(.opacity)
+                .zIndex(1)
+            }
+        }
     }
     
     private func handleScan(result: Result<ScanResult, ScanError>) {
         switch result {
         case .success(let scanResult):
-            ROOTPopUpView(
-                isPresent: $showingPopUp,
-                title: "출석 체크 완료!",
-                content: "출석 체크가 완료되었습니다."
-            )
-            .transition(.opacity)
-            .zIndex(1)
+            showingPopUp = true
         case .failure(let error):
             print("Failed Scan QR Code because of \(error)")
         }
