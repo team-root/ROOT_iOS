a0113689e17a73a18db555551e224378336a65d0
circle0802 <hawon9781@gmail.com>
2025-03-05 15:11:19 +0900

    feat :: Schedule NaviBar 구현

--- ROOT/ROOT/Presentation/Scenes/Schedule/ScheduleView.swift
+++ ROOT/ROOT/Presentation/Scenes/Schedule/ScheduleView.swift
@@ -1,11 +1,38 @@
 import SwiftUI
 
 struct ScheduleView: View {
+    @State private var clickAlertButton: Bool = false
+
     var body: some View {
-        VStack {
-            Text("Schedule Screen")
-                .font(.largeTitle)
-                .padding()
+        NavigationView {
+            ZStack {
+                Color.gray600
+                    .ignoresSafeArea()
+                NavigationLink(isActive: $clickAlertButton) {
+                    AlertView()
+                } label: {
+                    EmptyView()
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("일정 확인")
+                        .rootFont(.body(.body1), color: .gray100)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button(action: {
+                        clickAlertButton = true
+                        print("alert button")
+                    }) {
+                        Image("bell")
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
         }
     }
+}
+
+#Preview {
+    ScheduleView()
 }
